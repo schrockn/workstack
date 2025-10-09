8b27762ce4ac2c9d52a60b10c966cff1f2f5302f
Nick Schrock <schrockn@dagsterlabs.com>
2025-10-09 05:22:14 -0400

    shell-wrapper

--- src/workstack/shell_integration/bash_wrapper.sh
+++ src/workstack/shell_integration/bash_wrapper.sh
@@ -5,15 +5,15 @@ workstack() {
   # Don't intercept if we're doing shell completion
   [ -n "$_WORKSTACK_COMPLETE" ] && { command workstack "$@"; return; }
 
-  local output status
+  local output exit_status
   output=$(command workstack __shell "$@")
-  status=$?
+  exit_status=$?
 
   # Passthrough mode: run the original command directly
   [ "$output" = "__WORKSTACK_PASSTHROUGH__" ] && { command workstack "$@"; return; }
 
   # If __shell returned non-zero, error messages are already sent to stderr
-  [ $status -ne 0 ] && return $status
+  [ $exit_status -ne 0 ] && return $exit_status
 
   # Execute the shell script output (eval handles empty strings safely)
   eval "$output"
