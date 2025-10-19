c683f5976266e845b4d8a13da60e4c9e53b5d5f8
Nick Schrock <schrockn@dagsterlabs.com>
2025-10-19 17:30:19 -0400

    cp

--- tests/fakes/fake_gitops.py
+++
@@ -1,9 +0,0 @@
-"""Alias for gitops module to support legacy imports.
-
-This module re-exports FakeGitOps from gitops.py to maintain backwards
-compatibility with test files that use the 'fake_gitops' naming convention.
-"""
-
-from tests.fakes.gitops import FakeGitOps
-
-__all__ = ["FakeGitOps"]
